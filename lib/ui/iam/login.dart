77fb7065d5ccea884be663ce852f63c6635f0ee2
María Alexandra Ahuanari Murayari <U20201A836@UPC.EDU.PE>
2024-09-09 15:19:50 -0500

    feat(login): added login modification

--- lib/ui/iam/login.dart
+++ lib/ui/iam/login.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
 class Login extends StatefulWidget {
   const Login({super.key});
 
@@ -10,13 +9,45 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+  Color facebookButtonColor = Colors.white;
+  Color googleButtonColor = Colors.white;
+  Color appleButtonColor = Colors.white;
+  Color textColor = Color(0xFF241152); // Color del texto e imagen normal
+  Color backgroundColor = Color(0xFF241152); 
+   Color borderColor = Color(0xFF241152); 
+ 
+
+String selectedButton = ''; // Estado para el botón seleccionado
+
+  void onButtonPressed(String button) {
+    setState(() {
+      // Restablecer todos los colores a blanco
+      facebookButtonColor = Colors.white;
+      googleButtonColor = Colors.white;
+      appleButtonColor = Colors.white;
+
+      // Cambiar el color del botón seleccionado
+      if (button == 'facebook') {
+        facebookButtonColor = backgroundColor;
+        selectedButton = 'facebook'; // Guardar el botón seleccionado
+      } else if (button == 'google') {
+        googleButtonColor = backgroundColor;
+        selectedButton = 'google'; // Guardar el botón seleccionado
+      } else if (button == 'apple') {
+        appleButtonColor = backgroundColor;
+        selectedButton = 'apple'; // Guardar el botón seleccionado
+      }
+    });
+}
+  
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
+    return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topCenter,
+            begin: Alignment.center,
             end: Alignment.bottomCenter,
             colors: [
               Color(0xFF7654E7), // Color superior
@@ -24,131 +55,185 @@ class _LoginState extends State<Login> {
             ],
           ),
         ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20), // Padding horizontal
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch, // Hace que los botones se estiren
-            children: [
-              const Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Color(0xFF241152), // Color del texto
-                  fontWeight: FontWeight.bold, // Texto en negrita
-                ),
-                textAlign: TextAlign.center, // Centra el texto
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Flexible(
+              child: Image.asset(
+                'assets/login.png',
+                fit: BoxFit.cover,
+                width: double.infinity,
               ),
-              const SizedBox(height: 20), // Espaciado entre el texto y la imagen
-              Image.asset(
-                'assets/logo.png', // Carga tu imagen PNG aquí
-                height: 100, // Ajusta el tamaño según sea necesario
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding:  const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: [
+                  const Text(
+                    '¡Hola!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 24,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Image.asset(
+                    'assets/back_hand.png',
+                    height: 30,
+                    width: 30,
+                  ),
+                ],
               ),
-              const SizedBox(height: 20), // Espaciado entre la imagen y el texto
-              const Text(
+            ),
+            const SizedBox(height: 8),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
                 'Empieza a experimentar la tranquilidad ahora',
                 style: TextStyle(
-                  color: Colors.white, // Color del texto
-                  fontWeight: FontWeight.bold, // Texto en negrita
-                  fontSize: 16, // Tamaño de la fuente
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 36,
+                  height: 1.3,
                 ),
-                textAlign: TextAlign.center, // Centra el texto
+                textAlign: TextAlign.left,
               ),
-              const SizedBox(height: 10), // Espaciado entre el texto y la línea
-              const Center(
-                child: Text(
-                  'Login with',
-                  style: TextStyle(
-                    color: Colors.white, // Color del texto
-                    fontWeight: FontWeight.bold, // Texto en negrita
+            ),
+            const SizedBox(height: 10),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.white,
+                      thickness: 2,
+                    ),
                   ),
-                  textAlign: TextAlign.center, // Centra el texto
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      'Login with',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.white,
+                      thickness: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FilledButton.icon(
+                      onPressed: () {
+                        onButtonPressed('facebook');
+                      },
+                      icon: SvgPicture.asset(
+                        facebookButtonColor == backgroundColor
+                            ? 'assets/facebook_blanco.svg' // Cambia la imagen a blanco
+                            : 'assets/facebook_morado.svg', // Imagen normal
+                        height: 20,
+                      ),
+                      label: Text(
+                        'Facebook',
+                        style: TextStyle(
+                          color: facebookButtonColor == backgroundColor
+                              ? Colors.white // Cambia el color del texto a blanco
+                              : textColor, // Color normal
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: facebookButtonColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        fixedSize: const Size(250, 45),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    FilledButton.icon(
+                      onPressed: () {
+                        onButtonPressed('google');
+                      },
+                      icon: SvgPicture.asset(
+                        googleButtonColor == backgroundColor
+                            ? 'assets/google_blanco.svg'  // Cambia la imagen a blanco
+                            : 'assets/google_morado.svg', // Imagen normal
+                        height: 20,
+                      ),
+                      label: Text(
+                        'Google',
+                        style: TextStyle(
+                          color: googleButtonColor == backgroundColor
+                              ? Colors.white // Cambia el color del texto a blanco
+                              : textColor, // Color normal
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      style: FilledButton.styleFrom(
+                        backgroundColor: googleButtonColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        fixedSize: const Size(250, 45),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    FilledButton.icon(
+                      onPressed: () {
+                        onButtonPressed('apple');
+                      },
+                      icon: SvgPicture.asset(
+                        appleButtonColor == backgroundColor
+                            ? 'assets/apple_blanco.svg' // Cambia la imagen a blanco
+                            : 'assets/apple_morado.svg', // Imagen normal
+                        height: 20,
+                      ),
+                      label: Text(
+                        'Apple ID',
+                        style: TextStyle(
+                          color: appleButtonColor == backgroundColor
+                              ? Colors.white // Cambia el color del texto a blanco
+                              : textColor, // Color normal
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      style: FilledButton.styleFrom(
+                        backgroundColor: appleButtonColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        fixedSize: const Size(250, 45),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 10), // Espaciado entre el texto y la línea
-              const Divider(
-                color: Colors.white, // Color de la línea
-                thickness: 2, // Grosor de la línea
-              ),
-              const SizedBox(height: 20), // Espaciado entre la línea y los botones
-              OutlinedButton.icon(
-                onPressed: () {
-                  // Acción para el primer botón
-                },
-                icon: SvgPicture.asset(
-                  'assets/facebook_morado.svg', // Ícono de Facebook
-                  height: 20,
-                ),
-                label: const Text(
-                  'Facebook',
-                  style: TextStyle(
-                    color: Color(0xFF241152), // Color del texto
-                    fontWeight: FontWeight.bold, // Texto en negrita
-                  ),
-                ),
-                style: OutlinedButton.styleFrom(
-                  side: const BorderSide(color: Colors.white),
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // Bordes más cuadrados
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10), // Espaciado entre los botones
-              OutlinedButton.icon(
-                onPressed: () {
-                  // Acción para el segundo botón
-                },
-                icon: SvgPicture.asset(
-                  'assets/google_morado.svg', // Ícono de Google
-                  height: 20,
-                ),
-                label: const Text(
-                  'Google',
-                  style: TextStyle(
-                    color: Color(0xFF241152), // Color del texto
-                    fontWeight: FontWeight.bold, // Texto en negrita
-                  ),
-                ),
-                style: OutlinedButton.styleFrom(
-                  side: const BorderSide(color: Colors.white),
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10), // Espaciado entre los botones
-              OutlinedButton.icon(
-                onPressed: () {
-                  // Acción para el tercer botón
-                },
-                icon: SvgPicture.asset(
-                  'assets/apple_morado.svg', // Ícono de Apple
-                  height: 20,
-                ),
-                label: const Text(
-                  'Apple ID',
-                  style: TextStyle(
-                    color: Color(0xFF241152), // Color del texto
-                    fontWeight: FontWeight.bold, // Texto en negrita
-                  ),
-                ),
-                style: OutlinedButton.styleFrom(
-                  side: const BorderSide(color: Colors.white),
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
-
-    
-    }
-
+  }
 }
